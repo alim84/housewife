da6199fd22198aaba298a51a1efe77787d6d7caf
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-11-10 13:05:21 +0600

    Banner Part

--- src/pages/Topmenu.jsx
+++ src/pages/Topmenu.jsx
@@ -3,12 +3,13 @@ import { GrAnnounce } from "react-icons/gr";
 import { GiCook } from "react-icons/gi";
 import { CiMenuFries } from "react-icons/ci";
 import { IoMdArrowDropdown } from "react-icons/io";
+import DailyFood from "./DailyFood";
 
 const Topmenu = () => {
   return (
     <>
       <div className="container mx-auto bg-red-400 py-2 px-5 shadow-md shadow-red-400 ">
-        <div className="flex justify-between mb-16">
+        <div className="flex justify-center mb-16">
           <div className="flex gap-8 items-center">
             <div className="border-l-2 border-r-2 px-2 hover:text-white ">
               <FaHome className="text-2xl" />
@@ -20,7 +21,7 @@ const Topmenu = () => {
               অভিযোগ
             </div>
           </div>
-          <div>
+          <div className="ml-48">
             <ul className="flex gap-8">
               <li className="py-3 px-5 border-2 hover:bg-red-300 rounded-md">রেজিস্ট্রেশন</li>
               <li className="py-3 px-5 border-2 hover:bg-red-300 rounded-md">প্রবেশ</li>
@@ -35,26 +36,27 @@ const Topmenu = () => {
               <a href="">Browse Menu</a>
             </div>
             <div>
-              <IoMdArrowDropdown />
+              <IoMdArrowDropdown className="" />
             </div>
           </div>
           <div className="flex gap-5">
-            <button className="flex gap-2 items-center py-3 px-10 bg-white rounded-tl-lg rounded-br-lg">
-              <GrAnnounce /> অফার
+            <button className="flex gap-2 items-center py-3 px-10 bg-white rounded-tl-lg rounded-br-lg hover:text-red-400">
+              <GrAnnounce className="hover:text-red-400" /> অফার
             </button>
             <input
               className="py-3 px-32 rounded-lg"
               type="text"
               placeholder="Search Your Choice"
             />
-            <button className=" flex items-center gap-2 py-3 px-12 bg-white rounded-md">
+            <button className=" flex items-center gap-2 py-3 px-12 bg-white rounded-md hover:text-red-400">
               {" "}
-              <GiCook />
+              <GiCook className="hover:text-red-400" />
               গৃহিনী
             </button>
           </div>
         </div>
       </div>
+      <DailyFood/>
     </>
   );
 };
