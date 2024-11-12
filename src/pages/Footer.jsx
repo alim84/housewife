a449e10a4f9dd6d13dfb534352245bccf655e217
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-11-12 11:12:37 +0600

    footer update

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -17,6 +17,55 @@ const Footer = () => {
           <FaLinkedin />
           <FaXTwitter />
         </div>
+        <div className="flex justify-between">
+          <div className="w-[200px] text-pretty">
+            <h2>IN A BRIEF</h2>
+            <p>
+              Khaas food is an innovative agri-tech startup based in Bangladesh
+              with the vision to create a future where safe agro food is the
+              standard, not the exception. We envision and inspire positive
+              impact in the world where every individual has access to
+              responsibly sourced, safe and nutritious products at its
+              originality that support their health and the health of the
+              environment.
+            </p>
+            <div>
+              <h3>Head office:</h3>
+              <p>
+                Floor- 8, 15/1-15/2, Biswas Shawpnil, Jigatala, Dhaka 1209
+                khaasfood@gmail.com
+              </p>
+            </div>
+          </div>
+          <div>
+            <h2>CONTACT US</h2>
+            <p>
+              09612002255 01708183874 (WhatsApp Only) customercare@khaasfood.com
+              Corporate Sales & Retailer Only
+            </p>
+          </div>
+          <div>
+            <h2>JOIN OUR COMMUNITY</h2>
+            <p> Official FB Group</p>
+            <h2>VISIT OUR SHOP</h2>
+            <p> Official FB Group</p>
+            <h2>DOWNLOAD OUR APP</h2>
+            <p> Official FB Group</p>
+          </div>
+          <div>
+            <h2>USEFUL LINKS</h2>
+            <ul>
+              <li>Privacy Policy </li>
+              <li>Delivery Policy </li>
+              <li>Refund Policy</li>
+              <li>Corporate Deal</li>
+              <li>FAQs </li>
+              <li>About Us</li>
+              <li>Contact Us</li>
+              <li>Our Blog </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
