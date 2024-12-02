17f00bde442c7e72b6bc0b8ac337643ade7e41c3
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-12-02 15:37:37 +0600

    Slider add

--- src/pages/Cake.jsx
+++ src/pages/Cake.jsx
@@ -1,179 +1,126 @@
 import { useState } from "react";
 import { CiSearch } from "react-icons/ci";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Cake = () => {
-  
-
   let [Viewitem, setViewitem] = useState(false);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
 
   return (
     <>
-      <div className=" py-4">
+      <div className="py-4">
         <div className="container mx-auto  ">
           <div className="bg-red-300 py-5">
-            <h1 className="px-4 text-2xl text-gray-700">Cake Items</h1>
+            <h1 className="px-4 text-2xl text-gray-700">কেক আইটেম</h1>
           </div>
         </div>
+        <Slider {...settings}>
+          <div className="container mx-auto justify-center items-center">
+            <div className="w-[255px] h-[350px] bg-red-100 shadow-md shadow-red-300 hover:border-2  hover:border-green-500 ">
+              <div className="p-5">
+                <img
+                  className="w-[200px] h-[150px] rounded-md mb-5"
+                  src="Cake-1.jpg"
+                  alt=""
+                />
+                <button onClick={() => setViewitem(!Viewitem)}>
+                  {" "}
+                  <CiSearch className="absolute translate-x-[200px] translate-y-[-180px]" />
+                </button>
+                <h2 className="text-lg text-center font-bold text-red-800 mb-4">
+                  Product Name
+                </h2>
+                <div className="flex gap-4 justify-center mb-4">
+                  <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+                    {" "}
+                    $ 200
+                  </div>
+                  <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+                    {" "}
+                    Offer 20%
+                  </div>
+                </div>
+                <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
+                  ADD TO CART
+                </button>
+              </div>
+            </div>
+          </div>
 
-        <div className="container flex mx-auto  gap-4 mt-4 ">
-          <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300 hover:border-2  hover:border-green-500 ">
-            <div className="p-5">
-              <img
-                className="w-[200px] h-[150px] rounded-md mb-5"
-                src="Cake-1.jpg"
-                alt=""
-              />
-               <button onClick={() => setViewitem(!Viewitem)}>
-              {" "}
-              <CiSearch className="absolute translate-x-[200px] translate-y-[-180px]" />
-            </button>
-              <h2 className="text-lg text-center font-bold text-red-800 mb-4">
-                Product Name
-              </h2>
-              <div className="flex gap-4 justify-center mb-4">
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+          <div className="container mx-auto">
+            <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300 hover:border-2  hover:border-green-500 ">
+              <div className="p-5">
+                <img
+                  className="w-[200px] h-[150px] rounded-md mb-5"
+                  src="Cake-1.jpg"
+                  alt=""
+                />
+                <button onClick={() => setViewitem(!Viewitem)}>
                   {" "}
-                  $ 200
+                  <CiSearch className="absolute translate-x-[200px] translate-y-[-180px]" />
+                </button>
+                <h2 className="text-lg text-center font-bold text-red-800 mb-4">
+                  Product Name
+                </h2>
+                <div className="flex gap-4 justify-center mb-4">
+                  <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+                    {" "}
+                    $ 200
+                  </div>
+                  <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+                    {" "}
+                    Offer 20%
+                  </div>
                 </div>
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  Offer 20%
-                </div>
+                <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
+                  ADD TO CART
+                </button>
               </div>
-              <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
-                ADD TO CART
-              </button>
             </div>
           </div>
-          <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300  hover:border-2  hover:border-green-500">
-            <div className="p-5">
-              <img
-                className="w-[200px] h-[150px] rounded-md mb-5"
-                src="Cake-2.jpg"
-                alt=""
-              />
-              <h2 className="text-lg text-center font-bold text-red-800 mb-4">
-                Product Name
-              </h2>
-              <div className="flex gap-4 justify-center mb-4">
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+          <div className="container mx-auto">
+            <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300 hover:border-2  hover:border-green-500 ">
+              <div className="p-5">
+                <img
+                  className="w-[200px] h-[150px] rounded-md mb-5"
+                  src="Cake-1.jpg"
+                  alt=""
+                />
+                <button onClick={() => setViewitem(!Viewitem)}>
                   {" "}
-                  $ 200
+                  <CiSearch className="absolute translate-x-[200px] translate-y-[-180px]" />
+                </button>
+                <h2 className="text-lg text-center font-bold text-red-800 mb-4">
+                  Product Name
+                </h2>
+                <div className="flex gap-4 justify-center mb-4">
+                  <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+                    {" "}
+                    $ 200
+                  </div>
+                  <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
+                    {" "}
+                    Offer 20%
+                  </div>
                 </div>
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  Offer 20%
-                </div>
+                <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
+                  ADD TO CART
+                </button>
               </div>
-              <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
-                ADD TO CART
-              </button>
             </div>
           </div>
-          <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300 hover:border-2  hover:border-green-500">
-            <div className="p-5">
-              <img
-                className="w-[200px] h-[150px] rounded-md mb-5"
-                src="Cake-3.jpg"
-                alt=""
-              />
-              <h2 className="text-lg text-center font-bold text-red-800 mb-4">
-                Product Name
-              </h2>
-              <div className="flex gap-4 justify-center mb-4">
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  $ 200
-                </div>
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  Offer 20%
-                </div>
-              </div>
-              <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
-                ADD TO CART
-              </button>
-            </div>
-          </div>
-          <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300 hover:border-2  hover:border-green-500">
-            <div className="p-5">
-              <img
-                className="w-[200px] h-[150px] rounded-md mb-5"
-                src="Cake-6.jpg"
-                alt=""
-              />
-              <h2 className="text-lg text-center font-bold text-red-800 mb-4">
-                Product Name
-              </h2>
-              <div className="flex gap-4 justify-center mb-4">
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  $ 200
-                </div>
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  Offer 20%
-                </div>
-              </div>
-              <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
-                ADD TO CART
-              </button>
-            </div>
-          </div>
-          <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300  hover:border-2  hover:border-green-500">
-            <div className="p-5">
-              <img
-                className="w-[200px] h-[150px] rounded-md mb-5"
-                src="Cake-5.jpg"
-                alt=""
-              />
-              <h2 className="text-lg text-center font-bold text-red-800 mb-4">
-                Product Name
-              </h2>
-              <div className="flex gap-4 justify-center mb-4">
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  $ 200
-                </div>
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  Offer 20%
-                </div>
-              </div>
-              <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold ">
-                ADD TO CART
-              </button>
-            </div>
-          </div>
-          <div className="w-[250px] h-[350px] bg-red-100 shadow-md shadow-red-300  ">
-            <div className="p-5">
-              <img
-                className="w-[200px] h-[150px] rounded-md mb-5"
-                src="Cake-4.jpg"
-                alt=""
-              />
-              <h2 className="text-lg text-center font-bold text-red-800 mb-4">
-                Product Name
-              </h2>
-              <div className="flex gap-4 justify-center mb-4">
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  $ 200
-                </div>
-                <div className="w-[80px] h-[30px] bg-red-300 rounded-lg text-center font-bold pt-1">
-                  {" "}
-                  Offer 20%
-                </div>
-              </div>
-              <button className="flex mx-auto py-3 px-7 text-white font-bold bg-red-400 hover:text-red-500 hover:bg-red-300 hover:font-bold  ">
-                ADD TO CART
-              </button>
-            </div>
-          </div>
-        </div>
+        </Slider>
       </div>
 
-      
       {Viewitem && (
         <div className="absolute translate-x-[50%] translate-y-[-50%] bg-red-200 w-[800px] h-[500px] p-6 grid grid-cols-2 items-center">
           <button
